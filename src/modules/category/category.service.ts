54eb340d785722fc91d2d51a17f325c7dd0665ea
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2026-07-05 00:22:11 +0600

    fix: add toast on delete category

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -17,9 +17,14 @@ const getAllCategories = async () => {
 
 const deleteCategory = async (id: string) => {
   const category = await prisma.category.findUnique({ where: { id } });
+  if (!category) throw new ApiError(404, "Category not found");
 
-  if (!category) {
-    throw new ApiError(404, "Category not found");
+  const mealCount = await prisma.meal.count({ where: { categoryId: id } });
+  if (mealCount > 0) {
+    throw new ApiError(
+      400,
+      "Cannot delete category that has meals assigned to it",
+    );
   }
 
   return prisma.category.delete({ where: { id } });
